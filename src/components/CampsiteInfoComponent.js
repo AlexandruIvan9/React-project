0bb994ff2f35980ca503900e4cd0abddcd17f115
Alex <alexandruivan1995@gmail.com>
2021-04-05 21:15:14 -0500

    Week2 task2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
     constructor (props) {
         super (props);
         this.props = props;
     }
+    renderCampsite (campsite) {
+        return(
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
     render() {
         if (this.props.campsite) {
             return(
-                <div className="row"></div>
+                <div className="row">
+                   { this.renderCampsite(this.props.campsite) }
+                </div>
             );
         }
         else {
