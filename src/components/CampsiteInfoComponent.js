5c5dfb2f289924f0830addd80689f41ed196f3c4
Alex <alexandruivan1995@gmail.com>
2021-04-11 17:37:53 -0500

    update

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -10,31 +10,31 @@ class CampsiteInfo extends Component {
                 {
                     id: 0,
                     text: 'What a magnificent view!',
-                    author: '-- Tinus Lorvaldes',
+                    author: 'Tinus Lorvaldes',
                     date: 'Oct 25,2018'
                 },
                 {
                     id: 1,
                     text: 'The campground was beautiful, but the bugs could bite sometimes.',
-                    author: '-- Brennen Ech',
+                    author: 'Brennen Ech',
                     date: 'Jun 16,2017'
                 },
                 {
                     id: 2,
                     text: 'I caught a world-record-breaking trout here on my last visit.',
-                    author: '-- McKenzie Sebastian',
+                    author: 'McKenzie Sebastian',
                     date: 'Feb 18,2019'
                 },
                 {
                     id: 3,
                     text: 'A wonderful place to reconnect with nature.',
-                    author: '-- Jordan Runn',
+                    author: 'Jordan Runn',
                     date: 'Aug 04,2019'
                 },
                 {
                     id: 4,
                     text: 'The stars at night were magnificent!',
-                    author: '-- Ann Dabramov',
+                    author: 'Ann Dabramov',
                     date: 'Jul 23,2018'
                 }
 
@@ -62,7 +62,7 @@ class CampsiteInfo extends Component {
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
                     {comments.map(comment => <div key={comment.id}>{comment.text}
-                    <p>{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div> )}
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div> )}
                 </div>
             );
         }
@@ -76,7 +76,7 @@ class CampsiteInfo extends Component {
             return(
                 <div className="row">
                    { this.renderCampsite(this.props.campsite) }
-                   { this.renderComments(this.state.comments) }
+                   { this.renderComments(this.props.campsite.comments) }
                 </div>
             );
         }
